1d34521932274692385833d5dda067f05c07bcd5
Hasan Alibegić <hasan.alibegic@edu.fit.ba>
2017-02-18 15:07:59 +0100

    Updated sass package

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['concat', 'uglify', 'sass', 'watch']);
 };
